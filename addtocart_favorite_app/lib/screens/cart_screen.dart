dc1340bf901c8f10b820f4ecd678c679aa368a71
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2022-08-26 16:34:44 +0800

    design the cart screen

--- addtocart_favorite_app/lib/screens/cart_screen.dart
+++ addtocart_favorite_app/lib/screens/cart_screen.dart
@@ -11,7 +11,55 @@ class CartScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Cart Screen'),
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Total',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24.0,
+                    ),
+                  ),
+                  const Spacer(),
+                  Chip(
+                    label: const Text(
+                      '99.00',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Order Now'),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return const Card(
+                  elevation: 5.0,
+                  child: ListTile(
+                    title: Text('title'),
+                    subtitle: Text('amount'),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
