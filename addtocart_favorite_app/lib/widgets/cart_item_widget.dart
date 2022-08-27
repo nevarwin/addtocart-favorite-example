57f7b58ecf520d417daa2b4dd40408a95a291a2a
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2022-08-27 23:06:17 +0800

    create cart item widget

---
+++ addtocart_favorite_app/lib/widgets/cart_item_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CartItem extends StatelessWidget {
+  const CartItem({
+    Key? key,
+    required this.title,
+    required this.price,
+    required this.quantity,
+  }) : super(key: key);
+
+  final String title;
+  final String price;
+  final String quantity;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5.0,
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text('Price: $price'),
+        trailing: Text('$quantity x'),
+      ),
+    );
+  }
+}
